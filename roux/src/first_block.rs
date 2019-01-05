bc2e0f04e72de256398332585e63ec83d7bbca88
Joey Gouly <joey.gouly@gmail.com>
2019-01-05 15:32:23 +0000

    [roux] Store the pieces to solve in FBEdges/FBCorners
    This allows the solver to insert x rotations before solving the FB,
    which can lead to better solutions.

--- roux/src/first_block.rs
+++ roux/src/first_block.rs
@@ -6,7 +6,13 @@ use solver::index::{
 };
 
 /// Edges of the first block (DL, FL, BL).
-pub struct FBEdges;
+pub struct FBEdges(Face, Face, Face);
+
+impl Default for FBEdges {
+  fn default() -> Self {
+    FBEdges(Face::D, Face::L, Face::F)
+  }
+}
 
 impl Index for FBEdges {
   const NUM_ELEMS: u32 = 24 * 22 * 20;
@@ -14,7 +20,11 @@ impl Index for FBEdges {
   fn from_cube(&self, c: &Cube) -> u32 {
     generic_edge_index(
       &c,
-      &[(Face::D, Face::L), (Face::F, Face::L), (Face::B, Face::L)],
+      &[
+        (self.0, self.1),
+        (self.2, self.1),
+        (self.2.opposite(), self.1),
+      ],
     )
   }
 
@@ -23,14 +33,24 @@ impl Index for FBEdges {
     generic_edge_index_decode(
       &mut c,
       i,
-      &[(Face::B, Face::L), (Face::F, Face::L), (Face::D, Face::L)],
+      &[
+        (self.2.opposite(), self.1),
+        (self.2, self.1),
+        (self.0, self.1),
+      ],
     );
     c
   }
 }
 
 /// Corners of the first block (DLF, DBL).
-pub struct FBCorners;
+pub struct FBCorners(Face, Face, Face);
+
+impl Default for FBCorners {
+  fn default() -> Self {
+    FBCorners(Face::D, Face::L, Face::F)
+  }
+}
 
 impl Index for FBCorners {
   const NUM_ELEMS: u32 = 24 * 21;
@@ -38,7 +58,10 @@ impl Index for FBCorners {
   fn from_cube(&self, c: &Cube) -> u32 {
     generic_corner_index(
       &c,
-      &[(Face::D, Face::L, Face::F), (Face::D, Face::B, Face::L)],
+      &[
+        (self.0, self.1, self.2),
+        (self.0, self.2.opposite(), self.1),
+      ],
     )
   }
 
@@ -47,7 +70,10 @@ impl Index for FBCorners {
     generic_corner_index_decode(
       &mut c,
       i,
-      &[(Face::D, Face::B, Face::L), (Face::D, Face::L, Face::F)],
+      &[
+        (self.0, self.2.opposite(), self.1),
+        (self.0, self.1, self.2),
+      ],
     );
     c
   }
@@ -61,8 +87,8 @@ impl IDDFSInfo for FBInfo {
 
   fn is_solved(&self, state: &Self::State) -> bool {
     let c = Cube::solved();
-    let fbe = FBEdges;
-    let fbc = FBCorners;
+    let fbe = FBEdges::default();
+    let fbc = FBCorners::default();
     *state == (fbe.from_cube(&c), fbc.from_cube(&c))
   }
 
@@ -78,9 +104,23 @@ impl IDDFSInfo for FBInfo {
 }
 
 impl FBInfo {
+  pub fn get_indexes(&self, c: &Cube) -> (FBEdges, FBCorners) {
+    use cube::sticker_cube::CentrePos;
+    let fbe = FBEdges(
+      c.centres[CentrePos::D as usize],
+      c.centres[CentrePos::L as usize],
+      c.centres[CentrePos::F as usize],
+    );
+    let fbc = FBCorners(
+      c.centres[CentrePos::D as usize],
+      c.centres[CentrePos::L as usize],
+      c.centres[CentrePos::F as usize],
+    );
+    (fbe, fbc)
+  }
+
   pub fn get_state(&self, c: &Cube) -> <Self as IDDFSInfo>::State {
-    let fbe = FBEdges;
-    let fbc = FBCorners;
+    let (fbe, fbc) = self.get_indexes(&c);
     (fbe.from_cube(&c), fbc.from_cube(&c))
   }
 }
@@ -88,6 +128,7 @@ impl FBInfo {
 #[cfg(test)]
 mod tests {
   use super::*;
+  use cube::Move;
   use solver::iddfs::iddfs;
   use solver::index::exhaustive_index_check;
   use solver::pruning::gen_prune_table;
@@ -95,21 +136,21 @@ mod tests {
 
   #[test]
   fn exhaustive_fbe() {
-    exhaustive_index_check(&FBEdges);
+    exhaustive_index_check(&FBEdges::default());
   }
 
   #[test]
   fn exhaustive_fbc() {
-    exhaustive_index_check(&FBCorners);
+    exhaustive_index_check(&FBCorners::default());
   }
 
   #[test]
   fn basic_fb() {
     let c = Cube::solved();
-    let fbe = FBEdges;
+    let fbe = FBEdges::default();
     let e_table = gen_transition_table(&fbe);
     let e_ptable = gen_prune_table(&e_table, 7, fbe.from_cube(&c));
-    let fbc = FBCorners;
+    let fbc = FBCorners::default();
     let c_table = gen_transition_table(&fbc);
     let c_ptable = gen_prune_table(&c_table, 4, fbc.from_cube(&c));
     let info = FBInfo(e_table, c_table, e_ptable, c_ptable);
@@ -119,18 +160,40 @@ mod tests {
     assert!(solved);
 
     let mut c = Cube::solved();
-    c.do_move(cube::Move::Face(cube::Face::R, 1));
+    c.do_move(Move::Face(cube::Face::R, 1));
     let solved = iddfs(info.get_state(&c), &info, 0, &mut Vec::new());
     assert!(solved);
 
     let mut c = Cube::solved();
-    c.do_move(cube::Move::Face(cube::Face::D, 1));
+    c.do_move(Move::Face(cube::Face::D, 1));
     let solved = iddfs(info.get_state(&c), &info, 0, &mut Vec::new());
     assert!(!solved);
 
     let mut c = Cube::solved();
-    c.do_move(cube::Move::Face(cube::Face::D, 1));
+    c.do_move(Move::Face(cube::Face::D, 1));
     let solved = iddfs(info.get_state(&c), &info, 1, &mut Vec::new());
     assert!(solved);
+
+    let mut c = Cube::solved();
+    c.do_move(Move::Rotation(cube::Rotation::Y, 1));
+    let (fbe, fbc) = info.get_indexes(&c);
+    let solved = iddfs(
+      (fbe.from_cube(&c), fbc.from_cube(&c)),
+      &info,
+      0,
+      &mut Vec::new(),
+    );
+    assert!(solved);
+
+    let mut solution = Vec::new();
+    c.do_move(Move::Rotation(cube::Rotation::X, 1));
+    let solved = iddfs(
+      (fbe.from_cube(&c), fbc.from_cube(&c)),
+      &info,
+      1,
+      &mut solution,
+    );
+    assert!(solved);
+    assert_eq!(Move::Face(Face::L, 1), solution[0]);
   }
 }
