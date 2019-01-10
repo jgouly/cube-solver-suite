36d38ad8f17cd4988d24798210c1f275957abfa3
Joey Gouly <joey.gouly@gmail.com>
2019-01-10 18:10:39 +0000

    [web] Add FBSolution struct for passing solutions to JS
    This commit also sorts the solution by length, this also could
    be done on the JS side depending on how the UI evolves.

--- web-frontend/src/lib.rs
+++ web-frontend/src/lib.rs
@@ -1,13 +1,20 @@
 use cube::parse_moves;
 use cube::Cube;
 use lazy_static::lazy_static;
-use miniserde::json;
+use miniserde::{json, MiniSerialize};
 use roux::first_block::*;
 use solver::iddfs::iddfs;
 
 mod interop;
 
 use crate::interop::*;
+
+#[derive(MiniSerialize)]
+struct FBSolution {
+  moves: String,
+  len: usize,
+  dl: String,
+}
 
 lazy_static! {
   static ref FB_INFO: FBInfo = { FBInfo::new() };
@@ -23,28 +30,34 @@ pub fn solve_fb(s: JSString) {
   let scramble = parse_moves(&s.as_string()).unwrap();
   c.do_moves(&scramble);
 
-  for o in 0..24 {
+  for &o in cube::sticker_cube::EdgePos::natural_order() {
     let mut c = c;
-    c.do_moves(&roux::DL_ORIENTATIONS[o]);
+    c.do_moves(&roux::DL_ORIENTATIONS[o as usize]);
     let mut solution = Vec::with_capacity(10);
     for i in 0..10 {
       let solved = iddfs(info.get_state(&c), info, i, &mut solution);
       if solved {
         // A move is 1 or 2 characters, and a space between moves.
         // So allocate 3 * i, for the maximum solution length.
-        let solution_len_max = 3 * (i + roux::DL_ORIENTATIONS[o].len());
+        let solution_len_max =
+          3 * (i + roux::DL_ORIENTATIONS[o as usize].len());
         let mut ret = String::with_capacity(solution_len_max);
-        for m in roux::DL_ORIENTATIONS[o] {
+        for m in roux::DL_ORIENTATIONS[o as usize] {
           ret.push_str(&format!("{} ", m));
         }
-        for m in solution {
+        for m in &solution {
           ret.push_str(&format!("{} ", m));
         }
-        solutions.push(ret);
+        solutions.push(FBSolution {
+          moves: ret,
+          len: solution.len(),
+          dl: String::from(format!("{:?}", o)),
+        });
         break;
       }
     }
   }
 
+  solutions.sort_by_key(|a| a.len);
   stack_push_str(&json::to_string(&solutions));
 }
